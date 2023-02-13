0aec00cfa45969ce19037eb78afb34e43c390207
Xope <errchosep25@gmail.com>
2023-02-13 13:49:37 +0100

    Formateo de fechas usando luxon tut5

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'La pag d Josep amb Express', subtitle: 'Alejandro Acosta World Domination Crew' });
+  res.redirect("/catalog");
 });
 
 module.exports = router;
